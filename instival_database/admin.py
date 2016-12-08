c4fa7223e9fe2559ecc57899f684c44285ca02ab
JuliaHsu <hsu.bd100@gmail.com>
2016-12-08 12:41:07 +0000

    Admin done

--- instival_database/admin.py
+++ instival_database/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User
+from .models import  Festival
+from .models import Profile
+from .models import Post
+from .models import Comment
+
+admin.site.register(User)
+admin.site.register(Festival)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
